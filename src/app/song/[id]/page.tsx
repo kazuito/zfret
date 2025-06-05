dc585536190a429aea4af4fc0725b8dbd4bb7bce
Kazuma Ito <zarigani2x@gmail.com>
2025-06-05 21:47:07 +0900

    update chords styles

--- src/app/song/[id]/page.tsx
+++ src/app/song/[id]/page.tsx
@@ -17,26 +17,31 @@ const Page = async ({ params }: Props) => {
     <div className="p-6">
       <div>
         <div className="text-xl font-bold">{song.title}</div>
-        <Link href={song.artist.url}>{song.artist.name}</Link>
+        <Link className="text-muted-foreground" href={song.artist.url}>
+          {song.artist.name}
+        </Link>
       </div>
       <div className="mt-6">
         <div className="flex flex-col gap-2">
-          {song.lines.map((line, i) => (
-            <div key={i} className="flex items-end">
-              {line.map((part, j) => {
-                const hasChord = !!part.chord;
-                return (
-                  <div
-                    key={j}
-                    className={cn("flex flex-col", hasChord && "ml-1.5")}
-                  >
-                    {hasChord && <div className="-ml-1">{part.chord}</div>}
-                    <div className="opacity-60">{part.lyric}</div>
-                  </div>
-                );
-              })}
-            </div>
-          ))}
+          {song.lines.map((line, i) => {
+            const lineHasLyric = line.some((part) => part.lyric);
+            return (
+              <div key={i} className="flex items-end gap-2">
+                {line.map((part, j) => {
+                  return (
+                    <div key={j} className={cn("flex flex-col")}>
+                      {!!part.chord && (
+                        <div className="text-sm">{part.chord}</div>
+                      )}
+                      {lineHasLyric && (
+                        <div className="opacity-40 h-6">{part.lyric}</div>
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
