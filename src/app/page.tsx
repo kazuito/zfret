6c23a68e537eebb9a150b1f4d438397d0e8cc1be
Kazuma Ito <zarigani2x@gmail.com>
2025-06-06 13:42:52 +0900

    add heading for ranking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { fetchTopSongs } from "@/lib/song";
+import { TrophyIcon } from "lucide-react";
 import Link from "next/link";
 
 export default async function Home() {
@@ -6,19 +7,32 @@ export default async function Home() {
 
   return (
     <div className="p-6">
-      <div className="flex flex-col">
-        {topSongs.map((song, i) => {
-          return (
-            <Link
-              className="flex rounded-sm hover:bg-dimmed items-center gap-3 p-2"
-              href={song.url}
-              key={i}
-            >
-              <div className="font-semibold w-6 text-end">{i + 1}</div>
-              <div>{song.title}</div>
-            </Link>
-          );
-        })}
+      <div>
+        <div className="flex items-center gap-2 justify-center">
+          <TrophyIcon size={22} />
+          <div className="text-xl font-bold">Top Songs</div>
+        </div>
+        <div className="flex flex-col mt-4">
+          {topSongs.map((song, i) => {
+            return (
+              <Link
+                className="flex rounded-sm hover:bg-dimmed items-center gap-3 p-2"
+                href={song.url}
+                key={i}
+              >
+                <div className="font-semibold w-6 text-end">{i + 1}</div>
+                <div>{song.title}</div>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+      <div className="mt-6">
+        <div className="flex items-center gap-2 justify-center">
+          <TrophyIcon size={22} />
+          <div className="text-xl font-bold">Top Artists</div>
+        </div>
+        <div className="text-center">Coming soon...</div>
       </div>
     </div>
   );
