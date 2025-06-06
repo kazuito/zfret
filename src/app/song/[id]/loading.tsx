655ef2abd6160c279a0a52b1551fd1026038913a
Kazuma Ito <zarigani2x@gmail.com>
2025-06-06 14:01:06 +0900

    add skeleton on song page

---
+++ src/app/song/[id]/loading.tsx
@@ -0,0 +1,25 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+const Loading = () => {
+  return (
+    <div className="p-6">
+      <div>
+        <Skeleton className="h-7 w-64 mb-2" />
+        <Skeleton className="h-4 w-32" />
+      </div>
+      <div className="mt-8">
+        <div className="flex flex-col gap-2">
+          {Array.from({ length: 24 }).map((_, i) => (
+            <Skeleton
+              key={i}
+              className="h-6 mb-2"
+              style={{ width: `${Math.floor(Math.random() * 60) + 10}%` }}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
