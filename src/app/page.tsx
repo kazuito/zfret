1ac98d0530bd7c9c9b9a749fc0cddf8726a303af
Kazuma Ito <zarigani2x@gmail.com>
2025-08-02 16:31:56 +0900

    add list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
+import List from "@/components/list";
+import ListItem from "@/components/list-item";
 import { fetchTopArtists, fetchTopSongs } from "@/lib/song";
-import { TrophyIcon } from "lucide-react";
-import Link from "next/link";
 
 export default async function Home() {
   const topSongs = await fetchTopSongs();
@@ -8,48 +8,28 @@ export default async function Home() {
 
   return (
     <div className="p-6 max-w-3xl mx-auto">
-      <div>
-        <div className="flex items-center gap-2 justify-center">
-          <TrophyIcon size={22} />
-          <div className="text-xl font-bold">Top Songs</div>
-        </div>
-        <div className="flex flex-col mt-4">
-          {topSongs.map((song, i) => {
-            return (
-              <Link
-                className="flex rounded-sm hover:bg-dimmed items-center gap-3 p-2"
-                href={song.url}
-                key={i}
-              >
-                <div className="w-6 text-end">{i + 1}</div>
-                <div className="font-semibold">{song.title}</div>
-                <div className="text-sm text-muted-foreground">
-                  {song.artistName}
-                </div>
-              </Link>
-            );
-          })}
-        </div>
-      </div>
+      <List heading={"人気曲ランキング"}>
+        {topSongs.map((song, i) => (
+          <ListItem
+            key={i}
+            href={`/song/${song.id}`}
+            prefix={i + 1}
+            title={song.title}
+            description={song.artistName}
+          />
+        ))}
+      </List>
       <div className="mt-6">
-        <div className="flex items-center gap-2 justify-center">
-          <TrophyIcon size={22} />
-          <div className="text-xl font-bold">Top Artists</div>
-        </div>
-        <div className="flex flex-col mt-4">
-          {topArtists.map((artist, i) => {
-            return (
-              <Link
-                className="flex rounded-sm hover:bg-dimmed items-center gap-3 p-2"
-                href={artist.url}
-                key={i}
-              >
-                <div className="w-6 text-end">{i + 1}</div>
-                <div className="font-semibold">{artist.name}</div>
-              </Link>
-            );
-          })}
-        </div>
+        <List heading={"人気アーティストランキング"}>
+          {topArtists.map((artist, i) => (
+            <ListItem
+              key={i}
+              href={artist.url}
+              prefix={i + 1}
+              title={artist.name}
+            />
+          ))}
+        </List>
       </div>
     </div>
   );
---
+++ src/components/list-item.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+type Props = {
+  prefix?: React.ReactNode;
+  title?: string;
+  description?: string;
+  href: string;
+};
+
+const ListItem = ({ prefix, title, description, href }: Props) => {
+  return (
+    <Link
+      href={href}
+      className="py-3 px-4 flex items-center gap-3 bg-secondary/40 rounded-md border border-border/60"
+    >
+      {prefix && <div className="w-6 text-sm text-foreground/80">{prefix}</div>}
+      <div className="">{title}</div>
+      {description && (
+        <div className="text-sm text-foreground/60">{description}</div>
+      )}
+    </Link>
+  );
+};
+
+export default ListItem;
