012d35eef9f387a3e9722aa74d60c72565f3e61e
Kazuma Ito <zarigani2x@gmail.com>
2025-08-07 18:20:59 +0900

    add acknowledgment text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,9 +7,16 @@ const Footer = () => {
   return (
     <div className="border-t bg-secondary/20">
       <div className="max-w-3xl mx-auto p-6 min-h-40 flex items-center">
-        <Link href="/" className="font-semibold">
-          Z-FRET
-        </Link>
+        <div>
+          <Link href="/" className="font-semibold">
+            Z-FRET
+          </Link>
+          <div className="text-sm text-foreground/60 mt-1">
+            This app is using data from <Link href="https://www.ufret.jp" target="_blank">
+            www.ufret.jp
+            </Link>
+          </div>
+        </div>
         <div className="ml-auto flex items-center h-5 gap-2">
           <Button variant="ghost" asChild>
             <Link href="https://github.com/kazuito/zfret" target="_blank">
