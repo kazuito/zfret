0c211350d23b50765b183274589a5541d581bbe6
Kazuma Ito <zarigani2x@gmail.com>
2025-08-07 16:31:39 +0900

    fix heading link

--- src/components/page-heading.tsx
+++ src/components/page-heading.tsx
@@ -9,13 +9,15 @@ const PageHeading = ({ children, subtitle }: Props) => {
   return (
     <div className="my-10">
       <Link
-        href="/"
+        href=""
         className="font-semibold text-xl sm:text-2xl flex items-center gap-2 sm:gap-3 [&_svg]:size-5 sm:[&_svg]:size-6 w-fit"
       >
         {children}
       </Link>
       {subtitle && (
-        <div className="sm:text-lg text-foreground/60 mt-1 w-fit">{subtitle}</div>
+        <div className="sm:text-lg text-foreground/60 mt-1 w-fit">
+          {subtitle}
+        </div>
       )}
     </div>
   );
