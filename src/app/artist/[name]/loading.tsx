ac0da2801a21ef67ee07f696212221bf6080ff20
Kazuma Ito <zarigani2x@gmail.com>
2025-08-07 22:38:15 +0900

    update skeletons

--- src/app/artist/[name]/loading.tsx
+++ src/app/artist/[name]/loading.tsx
@@ -2,15 +2,11 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const Loading = () => {
   return (
-    <div className="max-w-3xl mx-auto p-6">
-      <div>
+    <div className="max-w-3xl mx-auto p-6 pt-0">
+      <div className="my-10">
         <Skeleton className="w-40 h-8" />
       </div>
-      <div className="mt-6 flex flex-col gap-4">
-        {Array.from({ length: 10 }).map((_, i) => (
-          <Skeleton key={i} className="w-full h-6" />
-        ))}
-      </div>
+      <Skeleton className="h-screen w-full" />
     </div>
   );
 };
--- src/app/song/[id]/loading.tsx
+++ src/app/song/[id]/loading.tsx
@@ -2,20 +2,13 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const Loading = () => {
   return (
-    <div className="p-6 pt-4 mx-auto max-w-3xl">
-      <div>
-        <Skeleton className="h-18 w-full" />
+    <div className="p-6 pt-0 mx-auto max-w-3xl">
+      <div className="my-10">
+        <Skeleton className="h-8 w-60" />
+        <Skeleton className="h-5.5 w-40 mt-2" />
       </div>
-      <div className="mt-8">
-        <div className="flex flex-col gap-2">
-          {Array.from({ length: 24 }).map((_, i) => (
-            <Skeleton
-              key={i}
-              className="h-6 mb-2"
-              style={{ width: `${Math.floor(Math.random() * 60) + 10}%` }}
-            />
-          ))}
-        </div>
+      <div className="mt-10">
+        <Skeleton className="h-screen w-full" />
       </div>
     </div>
   );
