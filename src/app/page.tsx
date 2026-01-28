0293af24188ded1a2632a63826f8b3f6c71b001d
Kazuma Ito <zarigani2x@gmail.com>
2026-01-29 01:15:09 +0900

    refactor: enhance UI responsiveness for lists and homepage layout, and remove unused header icons.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,11 +21,11 @@ export default async function Home() {
 
   return (
     <div className="mx-auto flex max-w-5xl flex-col p-6 pt-0">
-      <section className="flex flex-col items-center gap-6 py-32">
-        <div className="text-center text-3xl font-semibold tracking-tight text-balance md:text-4xl/6">
+      <section className="flex flex-col items-center gap-6 py-20 lg:py-28">
+        <div className="text-center text-3xl font-semibold tracking-tight text-balance lg:text-4xl/6">
           The Chord Library for Music Lovers
         </div>
-        <p className="text-center text-base text-balance md:text-lg">
+        <p className="text-center text-base text-balance lg:text-lg">
           Explore chords and lyrics from iconic Japan Hits
         </p>
         <div className="flex gap-3">
@@ -44,7 +44,7 @@ export default async function Home() {
         </div>
       </section>
       <section className="flex flex-col gap-6 pb-16 md:flex-row">
-        <List.Wrapper className="flex-1">
+        <List.Wrapper className="flex-1 shrink-0">
           <List.Header asChild>
             <Link href="/trending/songs" className="w-fit">
               <HugeiconsIcon icon={MusicNote02Icon} size={20} />
@@ -70,7 +70,7 @@ export default async function Home() {
             </List.FooterLink>
           </List.Footer>
         </List.Wrapper>
-        <List.Wrapper className="flex-1">
+        <List.Wrapper className="flex-1 shrink-0">
           <List.Header asChild>
             <Link href="/trending/artists" className="w-fit">
               <HugeiconsIcon icon={Mic01Icon} size={20} />
