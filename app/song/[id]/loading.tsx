e3b72d24ff2f4e1e00fc36382a9a8cbf9f6946ea
Kazuma Ito <zarigani2x@gmail.com>
2026-06-27 11:55:24 +0900

    update loading style

--- app/song/[id]/loading.tsx
+++ app/song/[id]/loading.tsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <div className="grid place-content-center py-48">
-      <div className="shimmer text-muted-foreground text-sm">
+      <div className="shimmer text-muted-foreground text-sm starting:blur-xs duration-400">
         Loading&hellip;
       </div>
     </div>
